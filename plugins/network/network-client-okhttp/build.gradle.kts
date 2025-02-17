f5f1bfd4b72ac7c0a0023afa21147123b24def07
Drew Carlson <awcarlson@protonmail.com>
2025-02-17 11:40:41 -0800

    Setup android target for okhttp module

--- plugins/network/network-client-okhttp/build.gradle.kts
+++ plugins/network/network-client-okhttp/build.gradle.kts
@@ -1,15 +1,28 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+
 plugins {
+    id("com.android.library")
     id("publish-library")
 }
 
 kotlin {
     jvmToolchain(JAVA_VERSION.majorVersion.toInt())
     jvm()
+    androidTarget()
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    applyDefaultHierarchyTemplate {
+        common {
+            group("jvmCommon") {
+                withAndroidTarget()
+                withJvm()
+            }
+        }
+    }
     sourceSets {
         all {
             explicitApi()
         }
-        jvmMain {
+        val jvmCommonMain by getting {
             dependencies {
                 implementation(libs.okhttp)
             }
